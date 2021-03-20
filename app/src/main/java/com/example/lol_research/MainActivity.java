0d1a89f0ea0f2ba235c59968536435a8f4aa537b
Vincent Bernet <vincent.bernet99@gmail.com>
2021-03-20 19:53:07 +0100

    API Key fixé + commentaire et rendu tout plus clean

--- app/src/main/java/com/example/lol_research/MainActivity.java
+++ app/src/main/java/com/example/lol_research/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_ICONID = "com.example.Lol_Research.MESSAGE3";
     public static final String EXTRA_SUMMONERLVL = "com.example.Lol_Research.MESSAGE4";
     public static final String EXTRA_SUMMONERID = "";
+    public static final String API_Key = "RGAPI-f7c2b622-f661-4d33-957a-9f710af6638c";
 
     public String summId = "";
 
@@ -51,59 +52,52 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /** Call when research button ("In game !") is taped by the user */
+    /** Call when research button ("Profile") is taped by the user */
     public void researchPlayer(View view) {
         Intent intent = new Intent(this, PlayerActivity.class);
 
 
-
-
-        // Instantiate the RequestQueue.
+        /** Instantiate the RequestQueue. */
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-        // first part : api url (with correct server region  [euw1 / or other] + Summoner_Name (typed in editText) + riot key api (change every 24 hours so need to specify it to our teacher)
-        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key=RGAPI-f1d78707-bb91-458d-96f0-0b972fd53058";
+        /** First part : api url (with correct server region [euw1 / or other] + Summoner_Name (typed in editText)
+         + riot key api (change every 24 hours so need to specify it to our teacher) */
+        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key="+API_Key;
 
-        // Request the JSON format of the summoner searched
+        /** Request the JSON format of the summoner searched */
        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     String SummonerName ="";
                     String SummonerLvl ="";
                     String SummonerIconID ="";
                     String SummonerID="";
-                    String  AccountID="";
-
+                    String AccountID="";
 
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
+                            /** We stock the data that interest us */
                             SummonerName =  response.getString("name");
                             SummonerLvl = response.getString("summonerLevel");
                             SummonerIconID = response.getString("profileIconId");
                             SummonerID = response.getString("id");
-                            String test = response.getString("puuid");
-
                             AccountID = response.getString("accountId");
-                            Toast.makeText(MainActivity.this, SummonerID, Toast.LENGTH_LONG).show();
-                            Toast.makeText(MainActivity.this, test, Toast.LENGTH_LONG).show();
 
 
-                            // pass the summoner name to the new activity
+                            /** pass those data to the new activity */
                             intent.putExtra(EXTRA_NAME, SummonerName);
                             intent.putExtra(EXTRA_ACCOUNTID, AccountID);
                             intent.putExtra(EXTRA_SUMMONERLVL, SummonerLvl);
                             intent.putExtra(EXTRA_ICONID, SummonerIconID);
                             intent.putExtra(EXTRA_SUMMONERID, SummonerID);
 
-                            // start the new activity
+                            /** start the new activity */
                             startActivity(intent);
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
+                        /** We then load the player icon if he return to the new activity */
                         Picasso.get().load("https://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/"+SummonerIconID+".png").placeholder(R.drawable.question_mark).into(Summoner_Icon);
-
-                        /*Summoner_Icon.setImageResource(R.drawable.newimage);*/
-
+                        /** Was gone (didn't exist in the app), we now set it to visible */
                         Summoner_Icon.setVisibility(View.VISIBLE);
                     }
                 }, new Response.ErrorListener() {
@@ -117,16 +111,15 @@ public class MainActivity extends AppCompatActivity {
         queue.add(jsonObjectRequest);
     }
 
+
+    /** Call when research button ("En jeu ?") is taped by the user */
     public void researchPlayerInGame(View view) {
         Intent intent = new Intent(this, PlayerActivity.class);
-
-
-
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         // first part : api url (with correct server region  [euw1 / or other] + Summoner_Name (typed in editText) + riot key api (change every 24 hours so need to specify it to our teacher)
-        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key=RGAPI-261e279f-b4b2-4733-bed7-8ec331ae2678";
+        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key="+API_Key;
 
         // Request the JSON format of the summoner searched
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
@@ -179,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
         queue.add(jsonObjectRequest);
         //We will now try to access another API with the ID we just got
 
-        String urlGame = "https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summId + "?api_key=RGAPI-261e279f-b4b2-4733-bed7-8ec331ae2678";
+        String urlGame = "https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summId + "?api_key="+API_Key;
 
         // Request the JSON format of the summoner searched
         JsonObjectRequest jsonObjectGameRequest = new JsonObjectRequest
@@ -230,7 +223,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-/*
+/** Other Method to make the request to riot Api (in string format or array format)
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -248,8 +241,7 @@ public class MainActivity extends AppCompatActivity {
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
     }}
-*/
-/*  Get Array response, not working with riot api
+  Get Array response, not working with riot api
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
@@ -272,4 +264,3 @@ public class MainActivity extends AppCompatActivity {
         queue.add(request);
     }
 }*/
-//Toast.makeText(MainActivity.this, "Searching the Summoner : "+Summoner_Name.getText().toString(), Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/example/lol_research/PlayerActivity.java
+++ app/src/main/java/com/example/lol_research/PlayerActivity.java
@@ -34,6 +34,7 @@ import java.net.URL;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    public static final String API_Key = "RGAPI-f7c2b622-f661-4d33-957a-9f710af6638c";
     TextView SummonerName;
     ImageView SummonerIcon;
     TextView SummonerLvl;
@@ -47,7 +48,7 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
 
 
-        // Get the Intent that started this activity and extract the string
+        /** Get the Intent that started this activity and extract the string */
         Intent intent = getIntent();
         String Pass_Name = intent.getStringExtra(MainActivity.EXTRA_NAME);
         String Pass_Lvl = intent.getStringExtra(MainActivity.EXTRA_SUMMONERLVL);
@@ -55,6 +56,7 @@ public class PlayerActivity extends AppCompatActivity {
         String Pass_SummonerID = intent.getStringExtra(MainActivity.EXTRA_SUMMONERID);
         String Pass_AccountID = intent.getStringExtra(MainActivity.EXTRA_ACCOUNTID);
 
+        /** Initiate our View Variable */
         SummonerName = findViewById(R.id.textViewName);
         SummonerIcon = findViewById(R.id.imageViewIcon);
         SummonerLvl = findViewById(R.id.textViewLvl);
@@ -62,34 +64,44 @@ public class PlayerActivity extends AppCompatActivity {
         SummonerWinrate= findViewById(R.id.textViewWinrate);
         SummonerTier = findViewById(R.id.textViewTier);
 
+        /** Set The first part of our Table Value : From the previous activity (which was connected to summonerV4 Riot's api */
         Picasso.get().load("https://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/"+Pass_IconID+".png").placeholder(R.drawable.question_mark).into(SummonerIcon);
         SummonerName.setText(Pass_Name);
         SummonerLvl.setText(Pass_Lvl);
 
-        // Instantiate the RequestQueue.
+        /** Instantiate the RequestQueue. */
         RequestQueue queue = Volley.newRequestQueue(PlayerActivity.this);
 
-        Toast.makeText(PlayerActivity.this, Pass_SummonerID, Toast.LENGTH_SHORT).show();
-        // first part : api url (with correct server region  [euw1 / or other] + Summoner_Name (typed in editText) + riot key api (change every 24 hours so need to specify it to our teacher)
-        String urlPlayer = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+Pass_SummonerID+"?api_key=RGAPI-f1d78707-bb91-458d-96f0-0b972fd53058";
+        /** First part : api url (with correct server region [euw1 / or other] + Summoner_Name (typed in editText)
+         + riot key api (change every 24 hours so need to specify it to our teacher) */
+        String urlPlayer = "https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+Pass_SummonerID+"?api_key="+API_Key;
 
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, urlPlayer, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 String Summoner_Tier = "";
-                String Summoner_Win = "", Summoner_Loose="", Summoner_Rank="";
+                String Summoner_Win = "", Summoner_Loose="", Summoner_Rank="", Summoner_Lp="";
                 try {
                     JSONObject CoolData = response.getJSONObject(0);
                     Summoner_Tier = CoolData.getString("tier");
                     Summoner_Win = CoolData.getString("wins");
                     Summoner_Loose = CoolData.getString("losses");
                     Summoner_Rank = CoolData.getString("rank");
+                    Summoner_Lp = CoolData.getString("leaguePoints");
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                SummonerTier.setText(Summoner_Tier);
-                SummonerRank.setText(Summoner_Rank);
-                SummonerWinrate.setText(Summoner_Win +"W "+Summoner_Loose +"L");
+                if (Summoner_Tier!="") {
+                    SummonerTier.setText(Summoner_Tier + " " + Summoner_Rank + " | " + Summoner_Lp + "points");
+                    SummonerWinrate.setText(Summoner_Win +"V | "+Summoner_Loose +"D");
+                    SummonerRank.setText("Midlaner");
+                }
+                else{
+                    SummonerWinrate.setText("0V | 0D");
+                    SummonerTier.setText("Non Classé");
+                    SummonerRank.setText("Inconnu");}
+
             }
         }, new Response.ErrorListener() {
             @Override
