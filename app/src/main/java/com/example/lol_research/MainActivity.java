57e628a654853ac5179d2871733cf172381ee40f
unknown <benoitor@hotmail.fr>
2021-03-20 05:47:11 +0100

    Added the button to check if a player is in game, still a bit buggy

--- app/src/main/java/com/example/lol_research/MainActivity.java
+++ app/src/main/java/com/example/lol_research/MainActivity.java
@@ -36,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_ICONID = "com.example.Lol_Research.MESSAGE3";
     public static final String EXTRA_SUMMONERLVL = "com.example.Lol_Research.MESSAGE4";
 
+    public String summId = "";
+
     EditText Summoner_Name;
     ImageView Summoner_Icon;
 
@@ -48,16 +50,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /** Call when research button ("Go !") is taped by the user */
+    /** Call when research button ("In game !") is taped by the user */
     public void researchPlayer(View view) {
         Intent intent = new Intent(this, PlayerActivity.class);
+
 
 
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         // first part : api url (with correct server region  [euw1 / or other] + Summoner_Name (typed in editText) + riot key api (change every 24 hours so need to specify it to our teacher)
-        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key=RGAPI-0313ba92-cbd2-4164-aef0-1a3d6aa1a47c";
+        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key=RGAPI-261e279f-b4b2-4733-bed7-8ec331ae2678";
 
         // Request the JSON format of the summoner searched
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
@@ -100,7 +103,118 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         queue.add(jsonObjectRequest);
-    }}
+    }
+
+    public void researchPlayerInGame(View view) {
+        Intent intent = new Intent(this, PlayerActivity.class);
+
+
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        // first part : api url (with correct server region  [euw1 / or other] + Summoner_Name (typed in editText) + riot key api (change every 24 hours so need to specify it to our teacher)
+        String url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + Summoner_Name.getText().toString() + "?api_key=RGAPI-261e279f-b4b2-4733-bed7-8ec331ae2678";
+
+        // Request the JSON format of the summoner searched
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    String SummonerName ="";
+                    String SummonerLvl ="";
+                    String SummonerIconID ="";
+                    String  SID ="";
+                    @Override
+                    public void onResponse(JSONObject response) {//maybe create another file for the fonction that will fetch acc info
+                        try {
+                            SummonerName =  response.getString("name");
+                            SummonerLvl = response.getString("summonerLevel");
+                            SummonerIconID = response.getString("profileIconId");
+                            SID = response.getString("id");
+                            //we get the link to get information about the player's game if he is currently playing
+                            //String urlGame = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + AccountID + "?api_key=RGAPI-261e279f-b4b2-4733-bed7-8ec331ae2678";
+                            /*try{
+                                response.
+                            }*/
+
+                            // pass the summoner name to the new activity
+                            intent.putExtra(EXTRA_NAME, SummonerName);
+                            intent.putExtra(EXTRA_ACCOUNTID, SID);
+                            intent.putExtra(EXTRA_SUMMONERLVL, SummonerLvl);
+                            intent.putExtra(EXTRA_ICONID, SummonerIconID);
+                            summId = SID;//we get the account's id
+                            // start the new activity
+
+                            //startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(MainActivity.this, SummonerName + " " + SummonerLvl + " Summ ID : " + summId, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, SummonerName + " " + SummonerLvl, Toast.LENGTH_SHORT).show();
+                        Picasso.get().load("https://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/"+SummonerIconID+".png").placeholder(R.drawable.question_mark).into(Summoner_Icon);
+
+                        /*Summoner_Icon.setImageResource(R.drawable.newimage);*/
+
+                        Summoner_Icon.setVisibility(View.VISIBLE);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(MainActivity.this,"Nom d'invocateur incorrect ou clef api déprécié", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        queue.add(jsonObjectRequest);
+        //We will now try to access another API with the ID we just got
+
+        String urlGame = "https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + summId + "?api_key=RGAPI-261e279f-b4b2-4733-bed7-8ec331ae2678";
+
+        // Request the JSON format of the summoner searched
+        JsonObjectRequest jsonObjectGameRequest = new JsonObjectRequest
+                (Request.Method.GET, urlGame, null, new Response.Listener<JSONObject>() {
+                    String SummonerName ="";
+                    String SummonerLvl ="";
+                    String SummonerIconID ="";
+                    String  AccountID="";
+                    String GameType = "";
+                    @Override
+                    public void onResponse(JSONObject response) {//maybe create another file for the fonction that will fetch acc info
+                        try {
+                            /*SummonerName =  response.getString("name");
+                            SummonerLvl = response.getString("summonerLevel");
+                            SummonerIconID = response.getString("profileIconId");
+                            AccountID = response.getString("accountId");*/
+                            GameType = response.getString("gameType");
+
+
+                            // pass the summoner name to the new activity
+                            /*intent.putExtra(EXTRA_NAME, SummonerName);
+                            intent.putExtra(EXTRA_ACCOUNTID, AccountID);
+                            intent.putExtra(EXTRA_SUMMONERLVL, SummonerLvl);
+                            intent.putExtra(EXTRA_ICONID, SummonerIconID);*/
+                            // start the new activity
+
+                            //startActivity(intent);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(MainActivity.this,  " Playing : " + GameType, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, SummonerName + " " + SummonerLvl, Toast.LENGTH_SHORT).show();
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(MainActivity.this,"L'invocateur n'est pas en partie ou clef api déprécié", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        queue.add(jsonObjectGameRequest);
+    }
+
+
+}
 
 
 
